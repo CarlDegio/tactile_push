d64dad25ebef1441a0afdb3e0cc7b301421e187a
CarlDegio <2536890272Carl@gmail.com>
2023-01-12 20:40:29 +0800

    fix p2p test

--- Test/test_kinematics.py
+++ Test/test_kinematics.py
@@ -1,20 +1,21 @@
-
 def test_p2p():
-
     import time
     import pybullet as p
     import pybulletX as px
     import Bullet.draw_debug as draw_debug
+    import numpy as np
 
-    px.init()
+    px.init(mode=p.DIRECT)  # mode=p.DIRECT/p.GUI
     robot = px.Robot("../Meshes/ur10_origin.urdf", use_fixed_base=True)
-
-    while True:
+    step = 0
+    desire_pos = np.array([0.5, 0.5, 1])
+    desire_quaternion = np.array([0, 0, 0, 1])
+    while step < 100:
         time.sleep(0.1)
 
         desired_joint_positions = p.calculateInverseKinematics(
-            robot.id, robot.get_joint_index_by_name("ee_fixed_joint"), [0.5, 0.5, 1] ,[0, 0, 0, 1],
-            )
+            robot.id, robot.get_joint_index_by_name("ee_fixed_joint"), desire_pos, desire_quaternion,
+        )
         # endEffectorLinkIndex, ee_link == ee_fixed_joint
         # global targetPosition, targetOrientation
         draw_debug.draw_frame(robot.get_joint_index_by_name("ee_fixed_joint"))
@@ -25,3 +26,8 @@ def test_p2p():
             targetPositions=desired_joint_positions
         )
         p.stepSimulation()
+        step += 1
+    real_ee_position, real_ee_orientation = \
+        p.getLinkState(robot.id, robot.get_joint_index_by_name("ee_fixed_joint"))[0:2]
+    assert np.allclose(real_ee_position, desire_pos, atol=0.001)
+    assert np.allclose(real_ee_orientation, desire_quaternion,atol=0.001)
