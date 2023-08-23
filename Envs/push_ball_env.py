67fef11d9bd27591f5c3174f471f6796cc6a413c
lzh <2536890272@qq.com>
2023-08-23 11:41:31 +0800

    fix plane ball push task

--- Envs/push_ball_env.py
+++ Envs/push_ball_env.py
@@ -1,5 +1,6 @@
 import os
 
+import cv2
 import gym
 import tacto
 from gym import spaces
@@ -14,13 +15,14 @@ from DigitUtil import depth_process
 class PushBallEnv0(gym.Env):
     metadata = {"render_modes": ["human", "none"]}
 
-    def __init__(self, render_mode=None, seed=None, dense_reward=False):
+    def __init__(self, render_mode=None, seed=None, dense_reward=False, tactile=True):
         self.step_repeat = 24
         self.max_step = 80
         self.np_random = None
         self.step_num = 0
         self.seed(seed)
         self.dense_reward = dense_reward
+        self.tactile = tactile
         project_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         assert render_mode is None or render_mode in self.metadata["render_modes"]
         if render_mode is None:
@@ -32,7 +34,8 @@ class PushBallEnv0(gym.Env):
             px.init(mode=p.GUI)
         else:
             px.init(mode=p.DIRECT)
-        self.robot = px.Robot(os.path.join(project_path, "Meshes/ur10_tactile.urdf"), use_fixed_base=True, flags=1)
+        self.robot = px.Robot(os.path.join(project_path, "Meshes/ur10_tactile.urdf"),
+                              use_fixed_base=True, flags=1, base_position=[-0.1, 0, 0])
         self.sphere = px.Body(os.path.join(project_path, "Meshes/sphere_small/sphere_small.urdf"),
                               base_position=[0.32, 0, 0.03],
                               use_fixed_base=False,
@@ -44,6 +47,7 @@ class PushBallEnv0(gym.Env):
 
         self.desire_pos = np.array([0.25, 0.0, 0.01])
         self.desire_quaternion = np.array([0, 0, 0, 1])
+        self.real_ball_pos = np.array([0.32, 0.0, 0.03])
         draw_debug.draw_frame(self.robot.get_joint_index_by_name("digit_joint"))
         draw_debug.draw_area(size=[0.05, 0.1, 0.05], position=[0.6, 0.0, 0])
         add_wall.add_walls()
@@ -51,54 +55,76 @@ class PushBallEnv0(gym.Env):
         # 2d end effort pos and vel, 2d ball pos and vel, 1d ball angular and vel
         self.observation_space = spaces.Dict(
             {
-                "x": spaces.Box(0.25, 0.68, shape=(1,), dtype=float),
+                "x": spaces.Box(0.24, 0.68, shape=(1,), dtype=float),
                 "y": spaces.Box(-0.65, 0.65, shape=(1,), dtype=float),
                 "angular": spaces.Box(-1, 1, shape=(1,), dtype=float),
                 "vx": spaces.Box(0, 0.12, shape=(1,), dtype=float),
                 "vy": spaces.Box(-0.12, 0.12, shape=(1,), dtype=float),
                 "vangular": spaces.Box(-0.5, 0.5, shape=(1,), dtype=float),
-                "ball_x": spaces.Box(0.3, 0.8, shape=(1,), dtype=float),
+                "ball_x": spaces.Box(0.2, 0.8, shape=(1,), dtype=float),
                 "ball_y": spaces.Box(-0.7, 0.7, shape=(1,), dtype=float),
-                "ball_vx": spaces.Box(0, 0.12, shape=(1,), dtype=float),
+                "ball_vx": spaces.Box(-0.12, 0.12, shape=(1,), dtype=float),
                 "ball_vy": spaces.Box(-0.12, 0.12, shape=(1,), dtype=float),
-                "tactile_mid": spaces.Box(0, 120, shape=(1,), dtype=float),
-                "tactile_sum": spaces.Box(0, 120 * 160 / 80, shape=(1,), dtype=float),
             }
         )
+        if self.has_tactile():
+            # self.observation_space["tactile_mid"] = spaces.Box(0, 120, shape=(1,), dtype=float)
+            # self.observation_space["tactile_sum"] = spaces.Box(0, 120 * 160 / 40, shape=(1,), dtype=float)
+            self.observation_space["rgb"] = spaces.Box(0, 255, shape=(64, 64, 3), dtype=float)
+            self.observation_space["dep"] = spaces.Box(0, 1, shape=(64, 64, 3), dtype=float)
 
         # end effort move
         self.action_space = spaces.Dict(
             {
-                "forward": spaces.Box(0, 0.0005, shape=(1,), dtype=float),
+                "forward": spaces.Box(-0.0001, 0.0005, shape=(1,), dtype=float),
                 "horizontal": spaces.Box(-0.0005, 0.0005, shape=(1,), dtype=float),
                 "rotate": spaces.Box(-0.002, 0.002, shape=(1,), dtype=float),
             }
         )
 
     def _get_obs(self):
-        color, depth = self.digits.render()
-        if self.render_mode == "human":
-            self.digits.updateGUI(color, depth)
-        self.depth_kit.update_depth(depth[0])
 
         real_pos, real_quaternion = get_state.get_ee_pose(self.robot)
         real_linear_velocity, real_angular_velocity = get_state.get_ee_vel(self.robot)
-        real_ball_pos = get_state.get_ball_pos(self.sphere)
+        self.real_ball_pos = get_state.get_ball_pos(self.sphere)
         real_ball_vel = get_state.get_ball_vel(self.sphere)
-        return {
+
+        obs = {
             "x": real_pos[0],
             "y": real_pos[1],
             "angular": p.getEulerFromQuaternion(real_quaternion)[2],
             "vx": real_linear_velocity[0],
             "vy": real_linear_velocity[1],
             "vangular": real_angular_velocity[2],
-            "ball_x": real_ball_pos[0],
-            "ball_y": real_ball_pos[1],
+            "ball_x": self.real_ball_pos[0],
+            "ball_y": self.real_ball_pos[1],
             "ball_vx": real_ball_vel[0],
             "ball_vy": real_ball_vel[1],
-            "tactile_mid": self.depth_kit.calc_center()[1],
-            "tactile_sum": self.depth_kit.calc_total(),
         }
+        if self.has_tactile():
+            color, depth = self.digits.render()
+            if self.render_mode == "human":
+                self.digits.updateGUI(color, depth)
+            self.depth_kit.update_depth(depth[0])
+            color_processed,depth_processed=self.process_image(color[0],self.depth_kit.depth)
+            obs.update({
+                # "tactile_mid": self.depth_kit.calc_center()[1],
+                # "tactile_sum": self.depth_kit.calc_total(),
+                "rgb": color_processed,
+                "dep": depth_processed
+            })
+
+        return obs
+
+    def process_image(self,color,depth):
+        color=color[:120,:,:]
+        color=cv2.resize(color,(64,64),cv2.INTER_NEAREST)
+        depth=depth[:120,:]
+        depth=cv2.resize(depth,(64,64),cv2.INTER_NEAREST)
+        depth=np.expand_dims(depth,axis=-1)
+        depth=np.broadcast_to(depth,(64,64,3))
+        # cv2.imshow("resize",color)
+        return color,depth
 
     def _get_info(self):
         return {
@@ -118,14 +144,19 @@ class PushBallEnv0(gym.Env):
         self.desire_quaternion = p.getQuaternionFromEuler([0, 0, desire_rotate])
 
     def reset(self, seed=None, options=None):
-        # TODO: some pose is not reachable
         start_y = self.np_random.uniform(low=-0.6, high=0.6)
         reset.reset_ur10_cartesian(self.robot, [0.25, start_y, 0.01], [0, 0, 0, 1])
-        reset.reset_ball_pos(self.sphere, [0.32, start_y, 0.03])
+        self.real_ball_pos = np.array([0.32, start_y, 0.03])
+        reset.reset_ball_pos(self.sphere, self.real_ball_pos)
+
         self.desire_pos = np.array([0.25, start_y, 0.01])
         self.desire_quaternion = np.array([0, 0, 0, 1])
 
+
         observation = self._get_obs()
+        if abs(observation['y']-observation['ball_y']) > 0.01:
+            warnings.warn("reset error: y != ball_y")
+            print("error observation:", observation)
         self.step_num = 0
 
         return observation
@@ -180,3 +211,9 @@ class PushBallEnv0(gym.Env):
 
     def close(self):
         pass
+
+    def has_tactile(self):
+        if self.tactile:
+            return True
+        else:
+            return False
